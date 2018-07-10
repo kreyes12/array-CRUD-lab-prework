06154bf7768cc16a4f22d76c5c4f053da1d7c0fd
Karla R <karla.y.reyes12@gmail.com>
2018-07-10 13:05:56 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,12 +6,12 @@ def create_an_array
   arr = [sock, sandal, shoe, slipper]
 end
 
-def add_element_to_end_of_array(arr, element)
-  arr.push(element)
+def add_element_to_end_of_array(array, element)
+  array.push(element)
 end
 
-def add_element_to_start_of_array(arr, element)
-  arr.unshift(element)
+def add_element_to_start_of_array(array, element)
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
