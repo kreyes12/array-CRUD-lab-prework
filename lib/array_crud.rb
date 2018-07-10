59a1b062176d0dae98e28ac809140fad3f2ac0db
Karla R <karla.y.reyes12@gmail.com>
2018-07-10 13:03:48 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [] = [sock, sandal, shoe, slipper]
+  arr = [sock, sandal, shoe, slipper]
 end
 
 def add_element_to_end_of_array(arr, element)
